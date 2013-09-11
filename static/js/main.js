b756f3e35316521216737e197bdab744da97a3e9
Damilare Onajole <web.dami@gmail.com>
2013-09-11 05:05:34 +0100

    can preview entries now

--- static/js/main.js
+++ static/js/main.js
@@ -3,24 +3,27 @@ $(document).ready(function(){
     var source_id = $('select[name=source_id]');
     var source_url = $('input[name=source_url]');
     var preview_btn = $('#preview-rule');
+    var preview_pane = $('#preview-pane table');
 
     // event handlers
     preview_btn.click(preview);
 
-    // functions
+    // preview button handler
     function preview(e) {
         e.preventDefault();
         rule_data = {'source_id': source_id.val(),
                      'source_url': source_url.val(),
                      'rules': rules.val()}
 
-        console.log(rule_data);
         $.post('/admin/rule/build', rule_data, function(data){
             display_preview(data);
         })
     }
 
+    // display entries in preview pane
     function display_preview(data){
-        console.log(data)
+        $.each(data.entries, function(index, value){
+            preview_pane.append('<tr><td><a href='+ value[0] + '>' + value[1] + '</a>')
+        })
     }
 });
